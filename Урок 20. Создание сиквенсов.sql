be7687259e7a30ab4f7ff056ffcab91ee9121868
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2026-01-26 21:09:21 +0300

    Урок 20 создан

---
+++ Урок 20. Создание сиквенсов.sql
@@ -0,0 +1,8 @@
+--SQL. Урок 20. Создание сиквенсов
+--https://rutube.ru/video/private/6f3625686eb310fe8788c2112ade94bd/?p=TyljlnGeLwqfuLNRn_bkbA
+
+--------------------------------------------------------------------------------
+CREATE SEQUENCE S_TOVAR
+MINVALUE 1
+MAXVALUE 9999999999999
+START WITH 1
