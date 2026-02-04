93cdbcfaf9352711f617e002fb00d11bcbf56e1f
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2026-02-04 19:38:49 +0300

    updated DZ 21 Urok

--- Урок 21. Создание представлений (VIEW).sql
+++ Урок 21. Создание представлений (VIEW).sql
@@ -73,7 +73,7 @@ SELECT p.personid
       , p.birthdate
       , (SELECT departamentname FROM departament WHERE p.departamentid = departamentid) as departamentname
 FROM persons p
-WHERE to_date(to_char(p.birthdate, 'dd.mm.') || EXTRACT(YEAR FROM SYSDATE), 'dd.mm.rrrr') > SYSDATE AND
+WHERE to_date(to_char(p.birthdate, 'dd.mm.') || EXTRACT(YEAR FROM SYSDATE), 'dd.mm.rrrr') >= SYSDATE AND
   to_date(to_char(p.birthdate, 'dd.mm.') || EXTRACT(YEAR FROM SYSDATE), 'dd.mm.rrrr') < SYSDATE + 15;
 ------------------------------------
 DROP VIEW incoming_birthdates_v;
