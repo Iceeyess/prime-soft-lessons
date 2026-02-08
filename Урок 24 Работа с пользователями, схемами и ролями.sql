98a1479bc60295a0b7e57685ad029850891413d3
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2026-02-08 13:31:17 +0300

    Домашнее задание выполнил (урок 24)

--- Урок 24 Работа с пользователями, схемами и ролями.sql
+++ Урок 24 Работа с пользователями, схемами и ролями.sql
@@ -1,5 +1,7 @@
 --https://rutube.ru/video/private/c2dccf26b65cbdb7c51c035393c48ffa/?p=Vv4gc3gNNLVt4haYjoD8eQ
 --SQL. Базы данных. ORACLE. Урок 24 Работа с пользователями, схемами и ролями
+--Для моей 21C версии OEBS новый подход с изменением паролей, ролей и т.п.
+
 /*
 Поскольку у меня БД контейнизированная через докер, то придется переключаться между 
 
@@ -125,3 +127,104 @@ SHOW CON_NAME;
 ```
 */
 
+SELECT *
+FROM user_sys_privs; -- все системные привилегии пользователя
+
+SELECT *
+FROM user_role_privs; -- все ролевые привилегии пользователя
+
+SELECT *
+FROM user_tab_privs;  -- все привилегии по объектам пользователя
+
+ -- для dba меняется user на dba, например, dba_sys_privs, dba_role_privs, dba_tab_privs - и т.д.
+
+SELECT *
+FROM SESSION_PRIVS; -- привилегии действующей сессии.
+
+/*
+Чтобы выдать все привилегии DBA для пользователя DIMA в Oracle 21c, выполните следующие шаги из учётной записи SYS:
+
+1. Подключение под SYS
+sql
+CONNECT SYS AS SYSDBA;
+-- или
+CONNECT / AS SYSDBA;
+2. Выдать роль DBA (рекомендуемый способ)
+sql
+GRANT DBA TO DIMA;
+-- Если нужно с возможностью делегирования:
+GRANT DBA TO DIMA WITH ADMIN OPTION;
+3. Сделать роль DBA ролей по умолчанию (опционально)
+sql
+ALTER USER DIMA DEFAULT ROLE DBA;
+4. Проверить выдачу привилегий
+sql
+-- Проверить роли пользователя DIMA
+SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTEE = 'DIMA';
+
+-- Проверить все привилегии в сессии
+CONNECT DIMA; -- подключиться под DIMA
+SELECT * FROM SESSION_PRIVS; -- посмотреть все привилегии
+SELECT * FROM SESSION_ROLES; -- посмотреть все активные роли
+*/
+
+--1. Сначала подключаемся как sysdba.
+--2. Меняем контейнер
+ALTER SESSION SET CONTAINER = XEPDB1;
+--3. Меняем пароль
+ALTER USER DIMA IDENTIFIED BY 1234;
+
+--4. Смотрим где мы:
+SHOW CON_NAME;
+--Если так: 
+------------------------------
+--XEPDB1
+
+--, то меняем обратно контейнер
+--5. 
+ALTER SESSION SET CONTAINER = CDB$ROOT;
+--6. Смотрим где мы:
+SHOW CON_NAME;
+--Должно быть так:
+--CON_NAME 
+------------------------------
+--CDB$ROOT
+
+--------------------------------------------------------------------------------
+--ГРАНТЫ
+
+--GRANT <какие права дать> ON <на какой объект> to <какому пользователю>;
+-- вместо пользователя можно дать права public - даются абсолютно всем пользователям, привер:
+--GRANT SELECT ON persons TO PUBLIC;
+
+--Убрать права через REVOKE 
+--REVOKE SELECT ON persons TO someusername;  -- у самого себя нельзя их отбирать, это тоже самое что съесть себя - не получится) 
+ 
+ 
+ -------------------------------------------------------------------------------
+--РОЛИ
+CREATE ROLE tester; --создание Роли
+-- снабжаем роль кучей прав
+GRANT ALL ON user0.persons TO tester;
+--или
+GRANT SELECT ON user0.canteendishes TO tester;
+-- Суть такая, что сначала создается роль, а потом к роли привязываются полномочия( то , что может делать внутри БД)
+GRANT tester to new_some_user; -- дается роль кому-то новому.
+REVOKE tester to some_user; -- убираются права
+
+--Практические задачи к Уроку 24
+--Домашка
+--1. Написать команду создания пользователя TEST с паролем test.  
+--2. Написать команду предоставления прав на чтение (SELECT) таблицы Persons пользователю TEST.  
+--3. Написать команду снятия привилегий на чтение (SELECT) таблицы Persons с пользователя TEST.  
+--4. Написать команду удаления пользователя TEST.
+--------------------------------------------------------------------------------
+--Не тестировал! Написал по памяти!!!!!!!!!!!!!!
+--1. выполнение 1 задания, но делают под правами DBA:
+CREATE USER test IDENTIFIED BY test;
+--2.
+GRANT SELECT ON persons TO test;
+--3.
+REVOKE SELECT ON persons TO test;
+--4.
+DROP USER test;
